83f93f9d8701bf68a3fd8ab8f929ad60e39e5e52
Mehakshami <mehakshami11@gmail.com>
2024-10-08 13:32:57 +0500

    Add files via upload

---
+++ mehak/index.js
@@ -0,0 +1,24 @@
+
+var multiply = (a, b, c) => {
+    return a * b * c;
+};
+console.log(multiply(5, 5, 5));
+
+var noreturn = () => {
+    return 'hello world'
+};
+console.log(noreturn());
+
+var colors = ["red", "blue", "green", "yellow", "purple"];
+var newArray = colors.slice(1, 4);
+console.log(newArray);
+
+let person1 = {
+    name: "MEHAK",
+    AGE: 17,
+    ROLLNO: {
+        rollnomehak: 271908,
+        rollnonasreen: 271097,
+    }
+};
+console.log(person1)
